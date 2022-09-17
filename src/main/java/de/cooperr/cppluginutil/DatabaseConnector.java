9128e48a1a994608cc39cd6eff7b2ebc1c158763
Cooperr06 <colin.rinke@googlemail.com>
2022-09-17 14:43:22 +0200

    Added JavaDocs for DatabaseConnector

--- src/main/java/de/cooperr/cppluginutil/DatabaseConnector.java
+++ src/main/java/de/cooperr/cppluginutil/DatabaseConnector.java
@@ -23,6 +23,15 @@ public class DatabaseConnector {
     private final MysqlDataSource dataSource = new MysqlConnectionPoolDataSource();
     private final PaperPlugin plugin;
     
+    /**
+     * Database connector for MySql
+     *
+     * @param host     address to the database
+     * @param port     port to connect
+     * @param database database to use
+     * @param user     user to be used for login
+     * @param password password to be used for login
+     */
     public DatabaseConnector(@NotNull PaperPlugin plugin, @Nullable String host, @Nullable Integer port, @NotNull String database,
                              @NotNull String user, @NotNull String password) {
         this.plugin = plugin;
@@ -36,6 +45,11 @@ public class DatabaseConnector {
         testDataSource(dataSource);
     }
     
+    /**
+     * Tests the database connection
+     *
+     * @param dataSource datasource to test
+     */
     private void testDataSource(DataSource dataSource) {
         try (Connection conn = dataSource.getConnection()) {
             if (!conn.isValid(1000)) {
@@ -47,6 +61,12 @@ public class DatabaseConnector {
         }
     }
     
+    /**
+     * Executes an sql file
+     *
+     * @param file file to execute
+     * @return whether an error occurred or not
+     */
     public boolean executeSqlFile(@NotNull File file) {
         String setup;
         
