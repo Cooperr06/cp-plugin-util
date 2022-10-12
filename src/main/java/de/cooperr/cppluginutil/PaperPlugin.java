163bd5fc5e2eca8c0c7f039473247ce166a480fd
Cooperr06 <colin.rinke@googlemail.com>
2022-10-12 14:22:34 +0200

    Added CustomConfig support

--- src/main/java/de/cooperr/cppluginutil/PaperPlugin.java
+++ src/main/java/de/cooperr/cppluginutil/PaperPlugin.java
@@ -1,5 +1,6 @@
 package de.cooperr.cppluginutil;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +9,8 @@ import org.jetbrains.annotations.NotNull;
  * Represents the java plugin with some utility methods
  */
 public abstract class PaperPlugin extends JavaPlugin {
+    
+    protected CustomConfig config;
     
     /**
      * Registers all commands by instantiating every {@link PaperCommand}.
@@ -46,5 +49,26 @@ public abstract class PaperPlugin extends JavaPlugin {
      */
     public <T extends Event> void registerListener(@NotNull PaperListener<T> listener) {
         getServer().getPluginManager().registerEvents(listener, this);
+    }
+    
+    /**
+     * Returns the custom config of this plugin
+     *
+     * @return custom config of this plugin
+     * @see CustomConfig
+     */
+    @Override
+    public @NotNull FileConfiguration getConfig() {
+        return config;
+    }
+    
+    /**
+     * Saves the custom config of this plugin
+     *
+     * @see CustomConfig#save()
+     */
+    @Override
+    public void saveConfig() {
+        config.save();
     }
 }
